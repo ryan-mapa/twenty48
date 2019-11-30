04eacd23adc0a1908fbbcccdaf2cea23d6a46cc2
ryan-mapa <ryan.m.mapa@gmail.com>
2019-11-29 20:40:50 -0800

    Test mobile swiping

--- source/game.js
+++ source/game.js
@@ -56,6 +56,59 @@ class Game {
                 this.debouncedHandleKey('down');
             }
         })
+
+        // mobile swiping code
+        document.addEventListener('touchstart', handleTouchStart, false);
+        document.addEventListener('touchmove', handleTouchMove, false);
+
+        let xDown = null;
+        let yDown = null;
+
+        function getTouches(evt) {
+            return evt.touches ||             // browser API
+                evt.originalEvent.touches; // jQuery
+        }
+
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches(evt)[0];
+            xDown = firstTouch.clientX;
+            yDown = firstTouch.clientY;
+        };
+
+        function handleTouchMove(evt) {
+            if (!xDown || !yDown) {
+                return;
+            }
+
+            let xUp = evt.touches[0].clientX;
+            let yUp = evt.touches[0].clientY;
+
+            let xDiff = xDown - xUp;
+            let yDiff = yDown - yUp;
+
+            if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+                if (xDiff > 0) {
+                    /* left swipe */
+                    alert('left!')
+                } else {
+                    /* right swipe */
+                    alert('right!')
+                }
+            } else {
+                if (yDiff > 0) {
+                    /* up swipe */
+                    alert('up!')
+                } else {
+                    /* down swipe */
+                    alert('down!')
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        };
+
+
     }
     
     handleKeyPress(direction) { 
